ea2ca2e1ec6026fc4cfaacb4a710eb7a76f9a12c
Sergey Tikhomirov <tihomirov.pro@gmail.com>
2023-10-01 23:42:39 +0300

    add query

---
+++ 12-query/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function queryString(object) {
+  const queries = [];
+
+  for (const key in object) {
+    queries.push(`${key}=${object[key]}`);
+  }
+
+  return queries.join('&');
+}
+
+console.log(queryString({search: 'Вася', take: 10}));
