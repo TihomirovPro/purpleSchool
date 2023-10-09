b2bed94e947981fde784d0ef75f05e27a4733270
Sergey Tikhomirov <tihomirov.pro@gmail.com>
2023-10-09 16:51:08 +0300

    add 1-map

---
+++ 1-map/index.js
@@ -0,0 +1,15 @@
+const arr = [
+  { id: 1, name: 'Вася' },
+  { id: 2, name: 'Петя' },
+  { id: 3, name: 'Андрей' },
+  { id: 1, name: 'Вася' },
+  { id: 3, name: 'Андрей' }
+];
+
+const newArr = arr.reduce((acc, el) => {
+  if (acc.find(item => item.id === el.id)) return acc;
+  acc.push(el);
+  return acc;
+}, []);
+
+console.log(new Set(newArr));
